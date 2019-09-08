d2b87f0459b602e2fcdec84a128d3595601d66a3
ruizema <mmrz33@gmail.com>
2019-09-07 21:47:34 -0400

    Allowed function to activate for all page changes

--- content.js
+++ content.js
@@ -1,29 +1,31 @@
 // Function that adds more playback speed options to the current video
 const addMoreSpeeds = () => {
-    const speedsMenu = document.getElementsByClassName("video-speeds menu")
+    const speedsMenu = document.getElementsByClassName("video-speeds menu")[0]
     const newSpeeds = ["2.0", "2.50", "3.0", "5.0"] // wanted speeds
-    for (let i = 0; i < speedsMenu.length; i++) {
-        for (let j = 0; j < newSpeeds.length; j++) {
+    if (speedsMenu.childNodes.length === 4) {
+        for (let i = 0; i < newSpeeds.length; i++) {
             let speed = document.createElement("li")
-            speed.setAttribute("data-speed", newSpeeds[j])
+            speed.setAttribute("data-speed", newSpeeds[i])
             let button = document.createElement("button")
             button.setAttribute("class", "control speed-option")
             button.setAttribute("tabindex", "-1")
             button.setAttribute("aria-pressed", "false")
-            button.innerHTML = newSpeeds[j] + "x"
+            button.innerHTML = newSpeeds[i] + "x"
             speed.appendChild(button)
-            speedsMenu[i].insertBefore(speed, speedsMenu[i].firstChild) // inserts each speed at the beginning of the menu
+            speedsMenu.insertBefore(speed, speedsMenu.firstChild) // inserts each speed at the beginning of the menu
         }
     }
 }
 
-// Add extra speed options for the first video on the page
-addMoreSpeeds()
-
 // Add extra speed options when switching videos
-const buttons = document.getElementsByTagName("Button")
-for (let button of buttons) {
-    if (button.getAttribute("class").startsWith("seq_video") || button.getAttribute("class").startsWith("sequence-nav-button")) {
-        button.onclick = addMoreSpeeds
-    }
-}
+const mutationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(addMoreSpeeds);
+});
+mutationObserver.observe(document.getElementById("seq_content"), {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+})
