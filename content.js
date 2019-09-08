d942ea95942357f1c936f0c678e13d5cde5c88d8
ruizema <mmrz33@gmail.com>
2019-09-07 21:01:44 -0400

    Fixed case when switching videos without reloading

--- content.js
+++ content.js
@@ -1,16 +1,29 @@
-// Accessing the speed options
-const speedsMenu = document.getElementsByClassName("video-speeds menu")[0]
+// Function that adds more playback speed options to the current video
+const addMoreSpeeds = () => {
+    const speedsMenu = document.getElementsByClassName("video-speeds menu")
+    const newSpeeds = ["2.0", "2.50", "3.0", "5.0"] // wanted speeds
+    for (let i = 0; i < speedsMenu.length; i++) {
+        for (let j = 0; j < newSpeeds.length; j++) {
+            let speed = document.createElement("li")
+            speed.setAttribute("data-speed", newSpeeds[j])
+            let button = document.createElement("button")
+            button.setAttribute("class", "control speed-option")
+            button.setAttribute("tabindex", "-1")
+            button.setAttribute("aria-pressed", "false")
+            button.innerHTML = newSpeeds[j] + "x"
+            speed.appendChild(button)
+            speedsMenu[i].insertBefore(speed, speedsMenu[i].firstChild) // inserts each speed at the beginning of the menu
+        }
+    }
+}
 
-// Generating the menu toggles for more speed options
-const newSpeeds = ["2.0", "2.50", "3.0", "5.0"]
-for (let i = 0; i < newSpeeds.length; i++) {
-    let speed = document.createElement("li")
-    speed.setAttribute("data-speed", newSpeeds[i])
-    let button = document.createElement("button")
-    button.setAttribute("class", "control speed-option")
-    button.setAttribute("tabindex", "-1")
-    button.setAttribute("aria-pressed", "false")
-    button.innerHTML = newSpeeds[i] + "x"
-    speed.appendChild(button)
-    speedsMenu.insertBefore(speed, speedsMenu.firstChild) // inserts each speed at the beginning of the menu
+// Add extra speed options for the first video on the page
+addMoreSpeeds()
+
+// Add extra speed options when switching videos
+const buttons = document.getElementsByTagName("Button")
+for (let button of buttons) {
+    if (button.getAttribute("class").startsWith("seq_video") || button.getAttribute("class").startsWith("sequence-nav-button")) {
+        button.onclick = addMoreSpeeds
+    }
 }
